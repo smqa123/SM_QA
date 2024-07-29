f0c47f3ed74cd485b183774c8330c2572f7c3328
SMQA_1 <118181301+smqa123@users.noreply.github.com>
2024-07-29 14:44:17 +0530

    Add files via upload

---
+++ ConcatString.sh
@@ -0,0 +1,9 @@
+# Prompting the user to enter two strings
+$string1 = "<#String1#>"
+$string2 = "<#String2#>"
+
+# Concatenating the strings
+$concatenatedString = "$string1$string2"
+
+# Displaying the concatenated string
+Write-Host "Concatenated string: $concatenatedString"
